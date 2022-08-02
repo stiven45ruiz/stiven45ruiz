8c92621a98ee026ad3754cf1e9b978f2191c03dd
stiven45ruiz <stiven45ruiz@gmail.com>
2022-08-02 12:47:25 -0500

    APOD

--- src/main/ AstronomyPicture/index.jsx
+++ src/main/ AstronomyPicture/index.jsx
@@ -17,7 +17,14 @@ const  AstronomyPicture = () => {
       <article className='pictureoftheday__container'>
         <h4 className='pictureoftheday__title'>{PictureOfTheDay.title}</h4>
         <img className='pictureoftheday__image' src={PictureOfTheDay.url} alt="" />
-
+        <p className='pictureoftheday__explanation'>
+          {PictureOfTheDay.explanation}
+        </p>
+        <a
+          className='pictureoftheday__link'
+          href="https://github.com/nasa/apod-api" 
+          target="_blank" 
+          rel="noreferrer"> Astronomy Picture of the Day</a>
       </article>
       
     </section>
